5497cc9a6cd1c14e33cd6a70f0077f564597b556
nishi <51656895+nishi@users.noreply.github.com>
2021-10-19 10:33:28 +0900

    Version1.0.0  リリース。 Version1.1.0  1.OleDbExcuteNonQueryの引数をList<string>型に変更。   1回の呼び出しで複数のSQLを実行できる。  2.OleDbExcuteNonQueryの戻り値をBoolean型に変更。   OleDbExcuteNonQueryの実行結果件数を取得はできなくなった。 Version1.2.0  処理中にエラーが発生した際に、メッセージボックスでエラー内容を表示するよう改修。 Version1.3.0  OleDbExcuteNonQueryメソッドの引数を可変長引数で指定できるよう改修。   設定例    1.引数を１つのみで指定可能。      OleDbExcuteNonQuery("insert")    2.引数を複数指定可能。      OleDbExcuteNonQuery("delete","update")    3.引数を配列で指定可能。      arr[0] = "insert"      arr[1] = "delete"      OleDbExcuteNonQuery(arr)    ※注     引数を何も指定しなかった場合でも処理が進む。その場合何もっせずコミットという動きになる。 Version1.3.1  不要なファイルをビルドしないよう修正。

---
+++ DBConnect/Class1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBConnect
+{
+    public class OleDBCommand
+    {
+        string _sqlConnect; //接続文字を格納します。
+
+        //コンストラクター
+        public OleDBCommand(String sqlConnect)
+            {
+                _sqlConnect = sqlConnect;
+            }
+
+        /// <summary>
+        /// 接続文字を格納します。
+        /// </summary>
+        //（WriteOnly Property)
+        public string SqlConnect
+        {
+            set
+            {
+                _sqlConnect = value;
+            }
+        }
+
+        /// <summary>
+        /// SQLコマンドを実行し、実行結果を取得します。
+        /// </summary>
+        /// <param name="sqlCommand">実行するSQL文（SELECT文）</param>
+        /// <returns></returns>
+        public DataTable OleDBDataTable(string sqlCommand)
+        {
+
+            System.Data.OleDb.OleDbConnection cn = new System.Data.OleDb.OleDbConnection(_sqlConnect);
+            System.Data.OleDb.OleDbCommand Com = new System.Data.OleDb.OleDbCommand(sqlCommand, cn);
+            DataTable respTable = new DataTable();
+
+            cn.Open();
+
+            try
+            {
+                respTable.Load(Com.ExecuteReader());
+                return respTable;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);                
+                return null;
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+        }
+
+        /// <summary>
+        /// SQLコマンドを実行します。
+        /// </summary>
+        /// <param name="sqlCommands">実行するSQL文（UPDATE,INSERT,DELETE文）</param>
+        /// <returns>正常終了:=true,異常終了:=false</returns>
+        public Boolean OleDbExcuteNonQuery(params string[] sqlCommands)
+        {
+            System.Data.OleDb.OleDbConnection cn = new System.Data.OleDb.OleDbConnection(_sqlConnect);
+            System.Data.OleDb.OleDbTransaction OleTran;
+            System.Data.OleDb.OleDbCommand Com = new System.Data.OleDb.OleDbCommand();
+            Com.Connection = cn;
+
+            cn.Open();
+            OleTran = cn.BeginTransaction();
+            Com.Transaction = OleTran;
+
+            try
+            {
+                for (int i1 = 0; i1 < sqlCommands.Length; i1++)
+                {
+                    Com.CommandText = sqlCommands[i1];
+                    Com.ExecuteNonQuery();
+                }
+
+                OleTran.Commit();
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);   
+                OleTran.Rollback();
+                return false;
+            }
+            finally
+            {
+                OleTran.Dispose();
+                cn.Close();
+            }
+
+        }
+
+        /// <summary>
+        /// SQLコマンドを実行し、実行結果を１つだけ取得します。
+        /// </summary>
+        /// <param name="sqlCommand">実行結果が１行１列となるSQL文</param>
+        /// <returns>実行結果（1つだけ返す。SQL文の集計関数等に有効）</returns>
+        public object OleDBExcuteScalar(string sqlCommand)
+        {
+            System.Data.OleDb.OleDbConnection cn = new System.Data.OleDb.OleDbConnection(_sqlConnect);
+            System.Data.OleDb.OleDbCommand Com = new System.Data.OleDb.OleDbCommand(sqlCommand,cn);
+            Object RespObject;
+
+            cn.Open();
+
+            try
+            {
+                RespObject = Com.ExecuteScalar();
+                return RespObject;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);   
+                return null;
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+    }
+}
